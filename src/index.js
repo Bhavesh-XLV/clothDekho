aec47d221d31decfadad1a02bef5e1752f37bd16
Bhavesh <bhaveshagrawal123.ba@gmail.com>
2022-05-14 13:55:06 +0530

    assignment 1

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { Provider as ReduxProvider } from "react-redux";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ReduxProvider>
+        <App />
+      </ReduxProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
