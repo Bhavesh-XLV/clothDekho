10bb9fb355a792247e0797045a4fb0fc76b2f4b0
Bhavesh <bhaveshagrawal123.ba@gmail.com>
2022-05-17 16:45:46 +0530

    assignment 2

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { Provider as ReduxProvider } from "react-redux";
+// import { Provider as ReduxProvider } from "react-redux";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <ReduxProvider>
-        <App />
-      </ReduxProvider>
+      {/* <ReduxProvider> */}
+      <App />
+      {/* </ReduxProvider> */}
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
